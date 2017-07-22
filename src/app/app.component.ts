b17e71fc6ede0089e131e4d18e3d5746a2938e0a
maazkabir <maazkabir@gmail.com>
2017-07-22 06:09:28 +0530

    Intro & SignUp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,40 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { HomePage } from '../pages/home/home';
+import firebase from 'firebase';
+
+
 @Component({
   templateUrl: 'app.html'
 })
+
+
 export class MyApp {
   rootPage:any = HomePage;
-
+	
+	
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+	
+	firebase.initializeApp({
+    apiKey: "AIzaSyAqxZ5NZNM2n97k2YcM2Xqsbu7CM7M7hHc",
+    authDomain: "trackomed-3671d.firebaseapp.com",
+    databaseURL: "https://trackomed-3671d.firebaseio.com",
+    projectId: "trackomed-3671d",
+    storageBucket: "trackomed-3671d.appspot.com",
+    messagingSenderId: "682956519321"
+});
+
+ platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      // Here you can do any higher level native things you might need.    
+	statusBar.styleDefault();
       splashScreen.hide();
-    });
+	
+
+});
   }
+
 }
 
+  
---
+++ src/pages/login/login.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { GooglePlus } from '@ionic-native/google-plus';
+import firebase from 'firebase';
+
+import { HomePage } from '../home/home';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  fireauth = firebase.auth();
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  userProfile: any = null;
+
+  constructor(public navCtrl: NavController, private googlePlus: GooglePlus) {
+    firebase.auth().onAuthStateChanged( user => {
+      if (user){
+        this.userProfile = user;
+      } else {
+        this.userProfile = null;
+      }
+    });
+  }
+
+	gotohome(){
+	this.navCtrl.setRoot(HomePage);
+	}
+
+  loginUser(): void {
+  this.googlePlus.login({
+    'webClientId': '682956519321-qc6vmccqiqjhceiuce3rna56hs8ikf75.apps.googleusercontent.com',
+    'offline': true
+  }).then( res => {
+    firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken))
+      .then( success => {
+        console.log("Firebase success: " + JSON.stringify(success));
+      })
+      .catch( error => console.log("Firebase failure: " + JSON.stringify(error)));
+    }).catch(err => console.error("Error: ", err));
+}
+
+}
